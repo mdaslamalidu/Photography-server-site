979b07a504329e58cf7ff138c3c4f0242fe072a8
Md Aslam Ali <mdaslamali675@gmail.com>
2022-11-08 19:00:38 +0600

    add new review item

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -22,11 +22,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
   const serviceCollection = client.db("photoService").collection("services");
+  const userReview = client.db("photoService").collection("review");
 
   app.get("/services", async (req, res) => {
     const query = {};
     const cursor = serviceCollection.find(query).limit(3);
     const result = await cursor.toArray();
+    res.send(result);
+  });
+
+  app.get("/allServices", async (req, res) => {
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+  });
+
+  app.get("/serviceDetails/:id", async (req, res) => {
+    const id = req.params;
+    console.log(id);
+    const query = { _id: ObjectId(id) };
+    const result = await serviceCollection.findOne(query);
+    res.send(result);
+  });
+
+  app.get("/review", async (req, res) => {
+    const query = {};
+    const cursor = userReview.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+  });
+
+  app.post("/review", async (req, res) => {
+    const query = req.body;
+    console.log(query);
+    const result = await userReview.insertOne(query);
     res.send(result);
   });
 }
